c41177c1fc7110220aa0359cff249c9128cc55c2
Eddy Gaspar <eddyg@Eddys-MBP.lan>
2022-01-26 15:15:06 -0800

    Completed budget tracker actvity

--- index.js
+++ index.js
@@ -2,4 +2,35 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    //ask user the first question from the weeklyExpenseQuestions array
+    //assign answer to variable; turn users answer from string to number
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    console.log(numberAnswer)
+
+    //Add users answer to weeklyExpenses total
+    weeklyExpenses = weeklyExpenses + numberAnswer
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    //ask user the first question from the monthlyExpenseQuestions array
+    //assign answer to variable; turn users answer from string to number
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    console.log(numberAnswer)
+
+    //Add users answer to weeklyExpenses total
+    monthlyExpenses = monthlyExpenses + numberAnswer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    //ask user the first question from the annualExpenseQuestions array
+    //assign answer to variable; turn users answer from string to number
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    console.log(numberAnswer)
+
+    //Add users answer to weeklyExpenses total
+    annualExpenses = annualExpenses + numberAnswer
+}
